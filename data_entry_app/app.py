21897c9b70dd4562df9d67d8b87cd16cfd281ed5
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2024-09-14 16:45:09 +0000

    Established nav pages as shiny module

--- data_entry_app/app.py
+++ data_entry_app/app.py
@@ -2,26 +2,58 @@
 import pandas as pd
 
 # Web/Visual frameworks
-from shiny import App, ui, render, reactive, types, req
+from shiny import App, ui, render, reactive, types, req, module
 
 # Shiny App Specific
 import database as db
 
+@module.ui
+def table_navigator_ui(prefix):
+    return ui.row(
+        ui.row(ui.column(2, ui.div(id=f"{prefix}_btn_update_placeholder")),
+            ui.column(8),
+            ui.column(2, ui.input_action_button(
+                "btn_new", "New", width="100%")),
+            ),
+        ui.output_data_frame("summary_table")
+    )
+
+@module.server
+def table_navigator_server(input, output, session, prefix, df):
+    updateButtonVisible = reactive.value(False)
+    df_selected_row = reactive.value(pd.DataFrame())
+
+    @render.data_frame
+    def summary_table():
+        return render.DataGrid(
+            df,
+            width="100%",
+            height="100%",
+            selection_mode="row"
+        )
+
+
+    @reactive.effect
+    def insert_update_button():
+        data_selected = summary_table.data_view(selected=True)
+        req(not (data_selected.empty or updateButtonVisible.get()))
+        ui.insert_ui(
+            ui.input_action_button("btn_update",
+                                   "Update", width='100%'),
+            selector=f"#{prefix}_btn_update_placeholder",
+            where="beforeBegin"
+        )
+        updateButtonVisible.set(True)
+        df_selected_row.set(data_selected.copy())
+
 app_ui = ui.page_fluid(
     ui.page_navbar(
         ui.nav_panel("New Practice Session", "New Practice Session - Input Form",
-                     ui.row(
-                         ui.row(ui.column(2, ui.div(id="placeholder")),
-                                ui.column(8),
-                                ui.column(2, ui.input_action_button(
-                                    "btn_new_session", "New", width="100%")),
-                                ),
-                         ui.output_data_frame("session_summary")
-                     )),
+            table_navigator_ui(id="practice_session", prefix="practice_session")),            
         ui.nav_panel("New Song", "New Song - Input Form",
-                     "Song"),
+            table_navigator_ui("song", prefix="song")),
         ui.nav_panel("New Artist", "New Artist - Input Form",
-                     "Artist"),
+            table_navigator_ui("artist", prefix="artist")),
         title="Guitar Study Tracker",
         id="page",
     ),
@@ -30,45 +62,8 @@ app_ui = ui.page_fluid(
 
 def server(input, output, session):
     
-    updateButtonVisible = reactive.value(False)
-    df_selected_row = reactive.value(pd.DataFrame())
-    
-
-    def nav_song():
-        return "Song"
-
-    def nav_artist():
-        return "Artist"
-
-    @render.data_frame
-    def session_summary():
-        return render.DataGrid(
-            db.df_summary_practice_sessions,
-            width="100%",
-            height="100%",
-            selection_mode="rows"
-        )
-
-    @reactive.effect
-    def insert_update_button():
-        data_selected = session_summary.data_view(selected=True)
-        req(not (data_selected.empty or updateButtonVisible.get()))
-        ui.insert_ui(
-            ui.input_action_button("btn_update_session",
-                                   "Update", width='100%'),
-            selector="#placeholder",
-            where="beforeBegin"
-        )
-        updateButtonVisible.set(True)
-        df_selected_row.set(data_selected.copy())
-
-    @render.ui
-    def page_manager():
-        if input.page() == "New Song":
-            return nav_song()
-        if input.page() == "New Artist":
-            return nav_artist()
-        return None
-
+    table_navigator_server("practice_session", prefix="practice_session", df=db.df_summary_practice_sessions)
+    table_navigator_server("song", prefix="song", df=db.df_summary_songs)
+    table_navigator_server("artist", prefix="artist", df=db.df_summary_artists)    
         
 app = App(app_ui, server)
