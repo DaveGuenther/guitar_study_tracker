9e2fb76f7956ecec41ccbd68f3dcd838d0ed5ac7
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2024-09-14 13:58:14 +0000

    using ui.insert to create update buttons

--- data_entry_app/app.py
+++ data_entry_app/app.py
@@ -1,15 +1,27 @@
+# Core
+import pandas as pd
 
 # Web/Visual frameworks
-from shiny import App, ui, render
+from shiny import App, ui, render, reactive, types, req
 
 # Shiny App Specific
 import database as db
 
 app_ui = ui.page_fluid(
     ui.page_navbar(
-        ui.nav_panel("New Practice Session", "New Practice Session - Input Form"),
-        ui.nav_panel("New Song", "New Song - Input Form"),
-        ui.nav_panel("New Artist", "New Artist - Input Form"),
+        ui.nav_panel("New Practice Session", "New Practice Session - Input Form",
+                     ui.row(
+                         ui.row(ui.column(2, ui.div(id="placeholder")),
+                                ui.column(8),
+                                ui.column(2, ui.input_action_button(
+                                    "btn_new_session", "New", width="100%")),
+                                ),
+                         ui.output_data_frame("session_summary")
+                     )),
+        ui.nav_panel("New Song", "New Song - Input Form",
+                     "Song"),
+        ui.nav_panel("New Artist", "New Artist - Input Form",
+                     "Artist"),
         title="Guitar Study Tracker",
         id="page",
     ),
@@ -18,25 +30,9 @@ app_ui = ui.page_fluid(
 
 def server(input, output, session):
     
-    @render.data_frame
-    def session_summary():
-        return render.DataGrid(
-            db.df_summary_practice_sessions,
-            width="100%",
-            height="100%",
-            selection_mode="row"
-        )
-
-    def nav_practice_session():
-        ret_ui = ui.row(
-            ui.row(
-                ui.column(6,ui.input_action_button("btn_update_session", "Update", width='100%')),
-                ui.column(6,ui.input_action_button("btn_new_session", "New", width="100%")),
-            ),
-            
-            ui.output_data_frame("session_summary")
-        )
-        return ret_ui
+    updateButtonVisible = reactive.value(False)
+    df_selected_row = reactive.value(pd.DataFrame())
+    
 
     def nav_song():
         return "Song"
@@ -44,14 +40,35 @@ def server(input, output, session):
     def nav_artist():
         return "Artist"
 
+    @render.data_frame
+    def session_summary():
+        return render.DataGrid(
+            db.df_summary_practice_sessions,
+            width="100%",
+            height="100%",
+            selection_mode="rows"
+        )
+
+    @reactive.effect
+    def insert_update_button():
+        data_selected = session_summary.data_view(selected=True)
+        req(not (data_selected.empty or updateButtonVisible.get()))
+        ui.insert_ui(
+            ui.input_action_button("btn_update_session",
+                                   "Update", width='100%'),
+            selector="#placeholder",
+            where="beforeBegin"
+        )
+        updateButtonVisible.set(True)
+        df_selected_row.set(data_selected.copy())
+
     @render.ui
     def page_manager():
-        if input.page()=="New Practice Session":
-            return nav_practice_session() 
-        if input.page()=="New Song":
+        if input.page() == "New Song":
             return nav_song()
-        if input.page()=="New Artist":
+        if input.page() == "New Artist":
             return nav_artist()
         return None
+
         
 app = App(app_ui, server)
