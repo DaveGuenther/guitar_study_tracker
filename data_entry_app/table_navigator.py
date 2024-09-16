e2efa41f42e524418166ccd654e4648e00e4b566
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2024-09-16 04:01:34 +0000

    Started modal for input form

--- data_entry_app/table_navigator.py
+++ data_entry_app/table_navigator.py
@@ -10,9 +10,9 @@ import data_processing
 @module.ui
 def nav_ui(shiny_data_payload:data_processing.ShinyTableModel):
     return ui.row(
-        ui.row(ui.column(2, ui.div(id=f"{shiny_data_payload.id}_btn_update_placeholder")),
-            ui.column(8),
-            ui.column(2, ui.input_action_button(
+        ui.row(ui.column(4, ui.div(id=f"{shiny_data_payload.id}_btn_update_placeholder")),
+            ui.column(4),
+            ui.column(4, ui.input_action_button(
                 "btn_new", "New", width="100%")),
             ),
         ui.output_data_frame("summary_table")
@@ -38,6 +38,21 @@ def nav_server(input, output, session, shiny_data_payload:data_processing.ShinyT
             selection_mode="row"
         )
 
+    @reactive.effect
+    @reactive.event(input.btn_update)
+    def triggerUpdateButton():
+        input_form_modal = ui.modal(
+            f"Update {shiny_data_payload.title} - Input Form",
+            ui.row(
+                ui.column(4, ui.input_action_button("btn_input_cancel","Cancel",width="100%")),
+                ui.column(4),
+                ui.column(4, ui.input_action_button("btn_input_form_submit", "Submit", width="100%")),
+            ),
+            title="Input Form",
+            easy_close=True,
+            footer=None,
+        )
+        ui.modal_show(input_form_modal)
 
     @reactive.effect
     def insert_update_button():
