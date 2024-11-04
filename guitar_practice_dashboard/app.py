b7bc73cfbc1ec6b07864b3219228748c3c70b0a4
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2024-11-03 22:01:25 -0500

    Starting to split apart shiny ui into separate files/functions

--- guitar_practice_dashboard/app.py
+++ guitar_practice_dashboard/app.py
@@ -7,9 +7,6 @@ import os
 import datetime
 import pytz
 from dotenv import load_dotenv
-import inspect
-
-#video_link="""<iframe width="560" height="315" src="https://www.youtube.com/embed/5xcqZiVpdy4?si=_Qak6xkMo-VblHun" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"""
 
 # Web/Visual frameworks
 from shiny import App, ui, render, reactive, types, req, module
@@ -20,13 +17,14 @@ from shiny.types import ImgData
 from faicons import icon_svg
 
 # App Specific Code
-import logger
-Logger = logger.FunctionLogger
+
 import orm # database models
 from database import DatabaseSession, DatabaseModel
 import data_prep
-import filter_shelf
+import ui_extension
 
+import logger
+Logger = logger.FunctionLogger
 Logger.setLogger(False) # turn off the shinylogger
 
 # pull database location and credential information from env variables
@@ -55,130 +53,17 @@ session_model.connect(user_name, pw, True)
 #session_model.df_raw= pd.read_csv('data.csv') # remove when done testing
 df_sessions, df_365 = data_prep.processData(session_model, song_model, artist_model, style_model)
 
-def sessions_filter_shelf():
-    songs = df_365[df_365['Song'].notna()]['Song'].unique()
-    ret_val = ui.div(
-        ui.h3("Filters:"),
-        ui.input_checkbox_group(
-            "song_title",
-            "Song Title",
-            choices={key:value for key,value in zip(songs, songs)},
-            selected=[key for key in songs],
-        ),
-        #ui.input_select(id='song_title',label="Song Title",choices=list(df_sessions['Song'].unique()),selected=list(df_sessions['Song'].unique()),multiple=True)
-    )
-    return ret_val
+
 
 app_ui = ui.page_fluid(
+        ui.HTML('<meta property="og:image" content="rosette-whole-small-min.png" />'),
         ui.tags.link(href='styles.css', rel="stylesheet"),
         ui.tags.link(href='flex.css', rel="stylesheet"),
         ui.div(
             ui.page_navbar(
+                ui_extension.tab_session(df_365),
                 # Execute ui code for shiny modules
-                ui.nav_panel("Practice Sessions", 
-                    ui.page_sidebar(
-                        ui.sidebar(
-                            sessions_filter_shelf(),
-                            open="closed",
-                        ),
-                        ui.card(
-                            ui.div(
-                                output_widget(id='waffle_chart'),
-                                ui.img(src='guitar-head-stock.png', height="240px"),
-                                id='guitar-neck-container',
-                            ).add_style('width:1750px; overflow-x: auto; display: flex; margin:0px; padding:0px;'),
-                            ui.div("* Indicates that a video recording was made that day.").add_style("text-align:right;"),
-                            class_="dashboard-card",
-                        ),
-                    
-                        ui.row(
-
-                            ui.column(6,
-                                ui.div(
-                                    ui.card(
-                                        ui.h3("Practice Session Notes (Past Week)"),#.add_style("color:#Ff9b15;"),
-                                        output_widget(id='last_week_bar_chart').add_style('height:200px; overflow-y: auto; display: flex;'),
-                                        ui.div(ui.output_data_frame(id="sessionNotesTable").add_class('dashboard-table')).add_style('max-height:200px; overflow-y: clip; display: flex;'),
-                                        ui.div("",class_='blank-fill-container'),
-                                        class_="dashboard-card",
-                                    ),
-                                ),
-                            ).add_style("padding-right:6px;"),
-
-                            ui.column(6,
-                                
-                                ui.card(
-                                    ui.div(
-                                        ui.h3("Time Spent Practicing Songs (Past Year)"),
-                                        output_widget(id='last_year_bar_chart'),
-                                        ui.div(class_='flex-blank'),
-                                    ).add_class("flex-vertical",)
-                                ).add_class("dashboard-card").add_style('overflow-y: auto; display: flex;'),
-                                ui.div(class_='flex-blank'),
-                                ui.h6(
-                                    ui.span("").add_class('flex-blank'),
-                                    #ui.div(ui.HTML(video_link)),
-                                    ui.tags.a("Dave Guenther",href="https://www.linkedin.com/in/dave-guenther-915a8425a",target='_blank'),
-                                    ", 2024",
-                                    ui.span("").add_style("width:5px; display:inline;"),  
-                                    "|",
-                                    ui.span("").add_style("width:5px; display:inline;"),  
-                                    "Source Code:",
-                                    ui.tags.a("GitHub",href="https://github.com/DaveGuenther/guitar_study_tracker",target='_blank'),
-                                    ui.span("").add_style("width:5px; display:inline;"),
-                                    "|",
-                                    ui.span("").add_style("width:5px; display:inline;"),  
-                                    "Data Source:",
-                                    ui.tags.a("Supabase",href="https://supabase.com/",target='_blank'),
-                                ).add_class('flex-horizontal').add_style('flex-wrap:wrap;'),
-                            ).add_class("flex-vertical").add_style("padding-left:6px;"),
-                        ).add_style("margin-top: 10px;"),    
-                            
-                        
-
-                    ),
-                ), 
-                ui.nav_panel("Career",
-                    "Career",
-                    ui.sidebar(
-                        ui.h4("Filters"),
-                        filter_shelf.filter_shelf(df_sessions),
-                        width=300,
-                    ),
-                    ui.row(
-                        ui.column(3,
-                            ui.card(
-                                ui.h4("Avg. Practice Time/Day"),
-
-                            ),
-                        ),
-                        ui.column(2,
-                            ui.card(
-                                ui.h4("Longest Practice Streak"),
-
-                            ),                        
-
-                        ),
-                        ui.column(2,
-                            ui.card(
-                                ui.h4("Longest Session"),
-
-                            ),
-                        ),
-                        ui.column(3,
-                            ui.card(
-                                ui.h4("Total Career Practice Time"),
-
-                            ),
-                        ),
-                        ui.column(2,
-                            ui.card(
-                                ui.h4("Year:"),
-
-                            ),
-                        ),                                                                                                
-                    ),
-                ), 
+                ui_extension.tab_career(df_sessions),
                 ui.nav_panel(
                     "Acoustic Arsenal",
                     "Acoustic Arsenal",
