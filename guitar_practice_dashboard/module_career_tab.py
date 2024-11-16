02f255f3e869155aa3e23a72e71deea789a5846c
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2024-11-15 23:39:16 -0500

    Updated make_trace function to provide inputs as series objects rather than a dataframe

--- guitar_practice_dashboard/module_career_tab.py
+++ guitar_practice_dashboard/module_career_tab.py
@@ -93,16 +93,32 @@ def career_server(input, output, session):
     def song_grindage_chart():
         print("Hello")
 
-        def make_traces(df_in, dimension_a, dimension_b, field_3, dimension_a_sort_order=None, dimension_b_sort_order=None):
+        def make_stacked_bar_traces_old(df_in, dimension_a, dimension_b, field_3, dimension_a_unique_sort_order=None, dimension_b_unique_sort_order=None):
             """
             dimension_a, dimension_b  (str): column name of a dimension in the incoming dataframe.  These will be the rows and columns of the matrix that is built.
             field_3 (str): column name of a measure or dimension in the dataframe who's value will be isolated in the intersection of dimension_a and dimension_b.
-            dimension_a_sort_order (list)
+            dimension_a_unique_sort_order (list): This is a list of the unique values of dimension_a sorted in any manner the usert wants. If omitted, it will use the data source order of unique values of dimension_a. 
+
+            Ex df_summary:
+            +--------+--------+----------+
+            | cat_a  | cat_b  |  measure |
+            +--------+--------+----------+
+            | Red    | Circle | 10       |
+            | Red    | Square | 5        |
+            | Green  | Diamond| 6        |
+            | Green  | Square | 11       |
+            | Purple | Circle | 3        |
+            | Purple | Diamond| 2        |
+            | Purple | Square | 6        |
+            | Blue   | Circle | 8        |
+            | Blue   | Diamond| 9        |
+            +--------+--------+----------+
+
             """
-            if dimension_a_sort_order:
-                df_in[dimension_a] = pd.Categorical(df_in[dimension_a], categories=dimension_a_sort_order, ordered=True)
-            if dimension_b_sort_order:
-                df_in[dimension_b] = pd.Categorical(df_in[dimension_b], categories=dimension_b_sort_order, ordered=True)
+            if dimension_a_unique_sort_order:
+                df_in[dimension_a] = pd.Categorical(df_in[dimension_a], categories=dimension_a_unique_sort_order, ordered=True)
+            if dimension_b_unique_sort_order:
+                df_in[dimension_b] = pd.Categorical(df_in[dimension_b], categories=dimension_b_unique_sort_order, ordered=True)
             df_in = df_in.sort_values([dimension_a,dimension_b])
             df_in_pivot = df_in.pivot(columns=[dimension_a],index=[dimension_b])
             dim_a_vals = list(df_in[dimension_a].unique())
@@ -113,9 +129,50 @@ def career_server(input, output, session):
             return dim_a_vals, dim_c_vals
             
 
+        def make_stacked_bar_traces(dimension_a, dimension_b, field_3, dimension_a_unique_sort_order=None, dimension_b_unique_sort_order=None):
+            """
+            dimension_a, dimension_b  (str): column name of a dimension in the incoming dataframe.  These will be the rows and columns of the matrix that is built.
+            field_3 (str): column name of a measure or dimension in the dataframe who's value will be isolated in the intersection of dimension_a and dimension_b.
+            dimension_a_unique_sort_order (list): This is a list of the unique values of dimension_a sorted in any manner the usert wants. If omitted, it will use the data source order of unique values of dimension_a. 
+
+            Ex df_summary:
+            +--------+--------+----------+
+            | cat_a  | cat_b  |  measure |
+            +--------+--------+----------+
+            | Red    | Circle | 10       |
+            | Red    | Square | 5        |
+            | Green  | Diamond| 6        |
+            | Green  | Square | 11       |
+            | Purple | Circle | 3        |
+            | Purple | Diamond| 2        |
+            | Purple | Square | 6        |
+            | Blue   | Circle | 8        |
+            | Blue   | Diamond| 9        |
+            +--------+--------+----------+
+
+            """
+            if not dimension_a_unique_sort_order:
+                dimension_a_unique_sort_order = list(dimension_a.unique())
+            if not dimension_b_unique_sort_order:
+                dimension_b_unique_sort_order = list(dimension_b.unique())
+
+            df_in = pd.DataFrame({'dim_a':dimension_a, 'dim_b':dimension_b, 'field_3':field_3})
+            
+            df_in['dim_a'] = pd.Categorical(df_in['dim_a'], categories=dimension_a_unique_sort_order, ordered=True)
+            df_in['dim_b'] = pd.Categorical(df_in['dim_b'], categories=dimension_b_unique_sort_order, ordered=True)
+            df_in = df_in.sort_values(['dim_a','dim_b'])
+            df_in_pivot = df_in.pivot(columns=['dim_a'],index=['dim_b'])
+            dim_a_uniques = list(df_in['dim_a'].unique())
+            dim_b_vals = list(df_in_pivot.index)
+            dim_c_vals=[]
+            for dim_b in dim_b_vals:
+                dim_c_vals.append(list(df_in_pivot.loc[dim_b]['field_3'].fillna(0)))
+            return dim_a_uniques, dim_c_vals
+
+
         stage_order = ['Learning Notes','Achieving Tempo','Phrasing','Maintenance']
         title_order = list(df_grindage.groupby('Title')['Duration'].sum().sort_values(ascending=False).index)
-        title_vals, duration_vals = make_traces(df_grindage, 'Title', 'Stage','Duration', dimension_a_sort_order=title_order, dimension_b_sort_order=stage_order)
+        title_vals, duration_vals = make_stacked_bar_traces(df_grindage['Title'], df_grindage['Stage'],df_grindage['Duration'], dimension_a_unique_sort_order=title_order, dimension_b_unique_sort_order=stage_order)
         
         data = [go.Bar(name=color, x = title_vals, y=duration) for color,duration in zip(stage_order,duration_vals)]
 
